2f9435dfbfbe5f42dbb5af1e62810a81891c2ce3
Ido Ben-Natan <ido.bennatan@gmail.com>
2014-11-13 15:10:46 +0200

    adding upload

--- www/src/10take.tabs/profile/profile.controller.js
+++ www/src/10take.tabs/profile/profile.controller.js
@@ -1,9 +1,42 @@
 (function() {
 
-	function ProfileCtrl(log) {
+	function ProfileCtrl(log, ionicModal, scope) {
 		log.debug('profile ctrl');
+		var _this = this;
+
+		// modal
+		ionicModal.fromTemplateUrl('src/10take.tabs/profile/modal.html', {
+	    scope: scope,
+	    animation: 'slide-in-up'
+	  }).then(function(modal) {
+	    _this.modal = modal;
+	  });
+	  _this.openModal = function() {
+	    _this.modal.show();
+	  };
+	  _this.closeModal = function() {
+	    _this.modal.hide();
+	  };
+	  //Cleanup the modal when we're done with it!
+	  scope.$on('$destroy', function() {
+	   	_this.modal.remove();
+	  });
+	  // Execute action on hide modal
+	  scope.$on('modal.hidden', function() {
+	    // Execute action
+	  });
+	  // Execute action on remove modal
+	  scope.$on('modal.removed', function() {
+	    // Execute action
+	  });
+
+
+
+
+
+
 	}
-		ProfileCtrl.$inject = ['$log']
+		ProfileCtrl.$inject = ['$log', '$ionicModal', '$scope']
 
 	angular.module('10take.tabs')
 		.controller('ProfileCtrl', ProfileCtrl)
